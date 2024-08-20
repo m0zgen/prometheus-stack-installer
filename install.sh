2385352d19c9b6fe6f16cec1aaa609914b7045ea
Yevgeniy Goncharov <g.yevgeniy.p@gmail.com>
2024-08-20 12:41:32 +0500

    + Skip install if already installed...

--- install.sh
+++ install.sh
@@ -138,98 +138,123 @@ firewall-cmd --reload
 # Installers
 installExporter() {
 
-    # Temporary catalog
-    cd $SCRIPT_PATH
-
-    # Temporary catalog
-    if [[ ! -d "$_install" ]]; then
-        mkdir $_install
+    # If /usr/local/bin/node_exporter exists
+    if [ -f /usr/local/bin/node_exporter ]; then
+        echo "Node Exporter already installed!"
+        echo "Skip..."
+        return
     else
-        rm -rf $_install; mkdir $_install
+        # Temporary catalog
+        cd $SCRIPT_PATH
+
+        # Temporary catalog
+        if [[ ! -d "$_install" ]]; then
+            mkdir $_install
+        else
+            rm -rf $_install; mkdir $_install
+        fi
+
+        cd $_install; 
+
+        # Check is wget installed
+        if ! type "wget" >/dev/null 2>&1; then
+            dnf install wget -y
+        fi
+
+        # Download Node Exporter
+        local _binary=`curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep browser_download_url | grep "linux-amd64" | awk '{print $2}' | tr -d '\"'`
+        wget $_binary; tar -xvf $(ls node_exporter*.tar.gz)
+        cd `ls -l | grep '.linux-amd[0-9]*$' | awk '{print $9}'`
+        cp node_exporter /usr/local/bin
+
+        # Create user
+        useradd --no-create-home --shell /bin/false node_exporter
+        chown node_exporter:node_exporter /usr/local/bin/node_exporter
+
+        # Setup systemd unit
+        setupNodeExporter
+
+        # User suggestion
+        echo -e "Add the following lines to /etc/prometheus/prometheus.yml:
+    - job_name: 'node_exporter'
+        scrape_interval: 5s
+        static_configs:
+        - targets: ['localhost:9100']
+
+        or just add to exist yml file to node_exporter section:
+        - targets: ['$_serverIP:9100'] 
+    "
+        echo "node_exporter is installed!"
+
     fi
-
-    cd $_install; 
-
-    # Check is wget installed
-    if ! type "wget" >/dev/null 2>&1; then
-        dnf install wget -y
-    fi
-
-    # Download Node Exporter
-    local _binary=`curl -s https://api.github.com/repos/prometheus/node_exporter/releases/latest | grep browser_download_url | grep "linux-amd64" | awk '{print $2}' | tr -d '\"'`
-    wget $_binary; tar -xvf $(ls node_exporter*.tar.gz)
-    cd `ls -l | grep '.linux-amd[0-9]*$' | awk '{print $9}'`
-    cp node_exporter /usr/local/bin
-
-    # Create user
-    useradd --no-create-home --shell /bin/false node_exporter
-    chown node_exporter:node_exporter /usr/local/bin/node_exporter
-
-    # Setup systemd unit
-    setupNodeExporter
-
-    # User suggestion
-    echo -e "Add the following lines to /etc/prometheus/prometheus.yml:
-  - job_name: 'node_exporter'
-    scrape_interval: 5s
-    static_configs:
-      - targets: ['localhost:9100']
-
-    or just add to exist yml file to node_exporter section:
-      - targets: ['$_serverIP:9100'] 
-"
-    echo "node_exporter is installed!"
 
 }
 
 installPrometheus() {
 
-    cd $SCRIPT_PATH; 
-
-    # Temporary catalog
-    if [[ ! -d "$_install" ]]; then
-        mkdir $_install
+    # If /usr/local/bin/prometheus exists
+    if [ -f /usr/local/bin/prometheus ]; then
+        echo "Prometheus already installed!"
+        echo "Skip..."
+        return
     else
-        rm -rf $_install; mkdir $_install
+
+        cd $SCRIPT_PATH; 
+
+        # Temporary catalog
+        if [[ ! -d "$_install" ]]; then
+            mkdir $_install
+        else
+            rm -rf $_install; mkdir $_install
+        fi
+
+        cd $_install; 
+        
+        # Check is wget installed
+        if ! type "wget" >/dev/null 2>&1; then
+            dnf install wget -y
+        fi
+        
+        # Download latest release
+        local _binary=`curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep browser_download_url | grep "linux-amd64" | awk '{print $2}' | tr -d '\"'`
+        wget $_binary; tar -xvf $(ls prometheus*.tar.gz)
+        cd `ls -l | grep '.linux-amd[0-9]*$' | awk '{print $9}'`
+
+        # User in catalogs creation 
+        useradd -m -s /bin/false prometheus
+        mkdir /var/lib/prometheus /etc/prometheus 
+        chown prometheus:prometheus /etc/prometheus
+        chown prometheus:prometheus /var/lib/prometheus
+
+        # Copy executable and set permissions
+        cp prometheus  /usr/local/bin
+        cp promtool  /usr/local/bin
+
+        chown prometheus:prometheus /usr/local/bin/prometheus
+        chown prometheus:prometheus /usr/local/bin/promtool
+
+        # Copy config
+        cp -r consoles /etc/prometheus
+        cp -r console_libraries /etc/prometheus
+        cp prometheus.yml /etc/prometheus/prometheus.yml
+
+        # Setup systemd unit
+        setupPrometheusSVC
+
+        echo "Prometheus installed!"
+
     fi
-
-    cd $_install; 
-    
-    # Check is wget installed
-    if ! type "wget" >/dev/null 2>&1; then
-        dnf install wget -y
-    fi
-    
-    # Download latest release
-    local _binary=`curl -s https://api.github.com/repos/prometheus/prometheus/releases/latest | grep browser_download_url | grep "linux-amd64" | awk '{print $2}' | tr -d '\"'`
-    wget $_binary; tar -xvf $(ls prometheus*.tar.gz)
-    cd `ls -l | grep '.linux-amd[0-9]*$' | awk '{print $9}'`
-
-    # User in catalogs creation 
-    useradd -m -s /bin/false prometheus
-    mkdir /var/lib/prometheus /etc/prometheus 
-    chown prometheus:prometheus /etc/prometheus
-    chown prometheus:prometheus /var/lib/prometheus
-
-    # Copy executable and set permissions
-    cp prometheus  /usr/local/bin
-    cp promtool  /usr/local/bin
-
-    chown prometheus:prometheus /usr/local/bin/prometheus
-    chown prometheus:prometheus /usr/local/bin/promtool
-
-    # Copy config
-    cp -r consoles /etc/prometheus
-    cp -r console_libraries /etc/prometheus
-    cp prometheus.yml /etc/prometheus/prometheus.yml
-
-    # Setup systemd unit
-    setupPrometheusSVC
-
-    echo "Prometheus installed!"
 }
 
 installGrafana() {
+
+    # If /etc/grafana/grafana.ini exists
+    if [ -f /etc/grafana/grafana.ini ]; then
+        echo "Grafana already installed!"
+        echo "Skip..."
+        return
+    else
+
 
     if [[ "$RPM" -eq "1" ]]; then
         echo -e "
@@ -244,7 +269,8 @@ sslverify=1
 sslcacert=/etc/pki/tls/certs/ca-bundle.crt
 " > /etc/yum.repos.d/grafana.repo
 
-yum install grafana -y
+    yum install grafana -y
+    
     else
         # Install Grafana for Debian
         echo "deb https://packages.grafana.com/oss/deb stable main" > /etc/apt/sources.list.d/grafana.list
@@ -254,41 +280,43 @@ yum install grafana -y
     fi
   
   
-sleep 15
-systemctl daemon-reload
-systemctl enable --now grafana-server
-sleep 5
+    sleep 15
+    systemctl daemon-reload
+    systemctl enable --now grafana-server
+    sleep 5
 
-grafana-cli plugins install grafana-piechart-panel
-sed -i 's/;admin_user/admin_user/g' /etc/grafana/grafana.ini
-sed -i 's/;admin_password/admin_password/g' /etc/grafana/grafana.ini
-sed -i 's/;disable_sanitize_html.*/disable_sanitize_html = true/g' /etc/grafana/grafana.ini
+    grafana-cli plugins install grafana-piechart-panel
+    sed -i 's/;admin_user/admin_user/g' /etc/grafana/grafana.ini
+    sed -i 's/;admin_password/admin_password/g' /etc/grafana/grafana.ini
+    sed -i 's/;disable_sanitize_html.*/disable_sanitize_html = true/g' /etc/grafana/grafana.ini
 
-NEWPASS=$(openssl rand -base64 12)
-grafana-cli --config "/etc/grafana/grafana.ini" admin reset-admin-password $NEWPASS
-systemctl restart grafana-server
-# systemctl status grafana-server
+    NEWPASS=$(openssl rand -base64 12)
+    grafana-cli --config "/etc/grafana/grafana.ini" admin reset-admin-password $NEWPASS
+    systemctl restart grafana-server
+    # systemctl status grafana-server
 
-if [[ "$1" -eq "auto" ]]; then
-    firewall-cmd --permanent --add-port=3000/tcp
-else
-    if confirm "Setup firewalld to INternal zone? (y/n or enter)"; then
-        firewall-cmd --permanent --add-port=3000/tcp --zone=internal
-    else
+    if [[ "$1" -eq "auto" ]]; then
         firewall-cmd --permanent --add-port=3000/tcp
+    else
+        if confirm "Setup firewalld to INternal zone? (y/n or enter)"; then
+            firewall-cmd --permanent --add-port=3000/tcp --zone=internal
+        else
+            firewall-cmd --permanent --add-port=3000/tcp
+        fi
     fi
+
+    firewall-cmd --reload
+
+    echo "Grafana installed!"
+    echo "Login: admin"
+    echo "Password: $NEWPASS"
+
+    # Save login data to file
+    echo "Login: admin" > /root/grafana_login.txt
+    echo "Password: $NEWPASS" >> /root/grafana_login.txt
+    echo "Login data saved to /root/grafana_login.txt"
+
 fi
-
-firewall-cmd --reload
-
-echo "Grafana installed!"
-echo "Login: admin"
-echo "Password: $NEWPASS"
-
-# Save login data to file
-echo "Login: admin" > /root/grafana_login.txt
-echo "Password: $NEWPASS" >> /root/grafana_login.txt
-echo "Login data saved to /root/grafana_login.txt"
 
 }
 
